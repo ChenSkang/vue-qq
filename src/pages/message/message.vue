55a4a4bd35dc636b51ae33849e33ab7e26cba60f
anknife <1981856494@qq.com>
2017-08-22 21:38:22 +0800

    路由修改和画面调整

--- src/pages/message/message.vue
+++ src/pages/message/message.vue
@@ -30,7 +30,18 @@
   <footers></footers>
   <div class="title">
     <span class="message">消息</span>
-    <span class="actions"><img src="../../img/add.png" /></span>
+    <span class="actions" @click="showfunction = !showfunction"><img src="../../img/add.png" /></span>
+  </div>
+  <div class="functions" v-if="showfunction">
+    <ul>
+      <li>创建群聊</li>
+      <li>加好友/群</li>
+      <li>扫一扫</li>
+      <li>面对面快传</li>
+      <li>付款</li>
+      <li>拍摄</li>
+      <li>面对面红包</li>
+    </ul>
   </div>
 </div>
 </template>
@@ -43,6 +54,7 @@
     },
     data () {
       return {
+        showfunction: false
       }
     },
     methods: {
@@ -67,5 +79,17 @@
     top: 65px;
     right: 25px;
     text-align: right;
+  }
+  .functions{
+    width: 45%;
+    position: absolute;
+    background-color: #fff;
+    top: 175px;
+    right: 20px;
+    border: 1px solid #8a8a8a;
+    border-radius: 10px;
+  }
+  .functions li{
+    margin: 20px 0 30px 25px;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,8 @@ import Router from 'vue-router'
 import Sign from '@/components/sign'
 import Message from '@/pages/message/message'
 import Contact from '@/pages/contact/contact'
-import Space from '@/pages/qqspace/space'
-import More from '@/pages/qqspace/more'
-import Friend from '@/pages/contact/friends/friends'
-import Group from '@/pages/contact/group/group'
-import Mynotes from '@/pages/contact/mynotes/mynotes'
-import Myphone from '@/pages/contact/myphone/myphone'
-import Ourchat from '@/pages/contact/ourchat/ourchat'
+import Space from '@/pages/space/space'
+import More from '@/pages/space/more'
 
 Vue.use(Router)
 
@@ -17,54 +12,22 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'sign',
       component: Sign
     },
     {
       path: '/message',
-      name: 'message',
       component: Message
     },
     {
       path: '/contact',
-      name: 'contact',
-      component: Contact,
-      children: [
-        {
-          path: '/friend',
-          name: 'friend',
-          component: Friend
-        },
-        {
-          path: '/group',
-          name: 'group',
-          component: Group
-        },
-        {
-          path: '/mynotes',
-          name: 'mynotes',
-          component: Mynotes
-        },
-        {
-          path: '/myphone',
-          name: 'myphone',
-          component: Myphone
-        },
-        {
-          path: '/ourchat',
-          name: 'ourchat',
-          component: Ourchat
-        }
-      ]
+      component: Contact
     },
     {
       path: '/space',
-      name: 'space',
       component: Space
     },
     {
       path: '/more',
-      name: 'more',
       component: More
     }
   ]
