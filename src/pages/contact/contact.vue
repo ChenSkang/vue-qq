f54138758f311152965dcd05824c1cd36aadd459
anknife <1981856494@qq.com>
2017-08-22 08:57:33 +0800

    contact

---
+++ src/pages/contact/contact.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <footers></footers>
+  <div class="title">
+    <span class="friends">联系人</span>
+    <span class="new-friends">添加</span>
+  </div>
+</div>
+</template>
+
+<script>
+  import footers from '../../components/footer.vue'
+  export default {
+    components: {
+      footers
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title span{
+    color: #fff;
+    font-family: PingFang SC;
+    font-size: 40px;
+    width: 100%;
+  }
+  .friends {
+    position: fixed;
+    top: 65px;
+    text-align: center;
+  }
+  .new-friends{
+    position: fixed;
+    top: 65px;
+    right: 25px;
+    text-align: right;
+  }
+</style>
