7ae6a06e00b1aabedcb2f4a18fa22c9ac1eb6a7c
anknife <1981856494@qq.com>
2017-08-22 08:57:58 +0800

    more

---
+++ src/pages/qqspace/more.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="title">
+      <span class="more">更多</span>
+      <router-link to="/qqspace">
+        <span class="friends">动态</span>
+      </router-link>
+    </div>
+    <div class="objects">
+      <ul>
+        <li class="list-title">已关闭的功能</li>
+        <li v-for="list in lists">
+          <div></div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        lists: [{
+          value: '日迹'
+        }]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title{
+    background-color: #20A0FF;
+    width: 100%;
+    height: 150px;
+    position: fixed;
+    top: 0;
+  }
+  .title span{
+    color: #fff;
+    font-family: PingFang SC;
+    font-size: 40px;
+    width: 100%;
+  }
+  .more {
+    position: fixed;
+    top: 65px;
+    text-align: center;
+  }
+  .friends{
+    position: fixed;
+    top: 65px;
+    left: 25px;
+    text-align: left;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Sign from '@/components/sign'
+import Message from '@/pages/message/message'
+import Contact from '@/pages/contact/contact'
+import Qqspace from '@/pages/qqspace/qqspace'
+import More from '@/pages/qqspace/more'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'sign',
+      component: Sign
+    },
+    {
+      path: '/message',
+      name: 'message',
+      component: Message
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    },
+    {
+      path: '/qqspace',
+      name: 'qqspace',
+      component: Qqspace
+    },
+    {
+      path: '/more',
+      name: 'more',
+      component: More
     }
   ]
 })
