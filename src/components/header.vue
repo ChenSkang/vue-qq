30119e9dce5d8576997e7c23c57b993b41978d3d
anknife <1981856494@qq.com>
2017-08-22 08:56:44 +0800

    header

---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="head">
+  <div class="account"><img src="../img/qqhead.jpg" /></div>
+</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.head{
+  background-color: #20A0FF;
+  width: 100%;
+  height: 150px;
+  position: fixed;
+  top: 0;
+}
+  .account{
+    width: 75px;
+    height: 75px;
+    border-radius: 50%;
+    overflow: hidden;
+    position: absolute;
+    left: 25px;
+    top: 50px;
+  }
+  .account img{
+    width: 75px;
+    height: 75px;
+  }
+</style>
