51e41e0549a0e44284985fdce264dd972dba5f31
anknife <1981856494@qq.com>
2017-08-23 15:38:50 +0800

    chat页面建立

---
+++ src/pages/chat/chat.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+  <div class="title">
+    <span class="more">{{username}}</span>
+    <router-link to="/message">
+      <div class="friends">
+        <img src="../../img/arrowleft.png" class="arrowleft" />
+      </div>
+    </router-link>
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title{
+    background-color: #20A0FF;
+    width: 100%;
+    height: 150px;
+    position: fixed;
+    top: 0;
+  }
+  .title span{
+    color: #fff;
+    font-family: PingFang SC;
+    position: fixed;
+    top: 75px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .arrowleft{
+    transform: translate(-30px);
+  }
+  .more {
+    position: fixed;
+    top: 65px;
+    text-align: center;
+  }
+  .friends{
+    position: fixed;
+    top: 65px;
+    left: 25px;
+  }
+</style>
